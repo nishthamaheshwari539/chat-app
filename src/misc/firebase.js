fea379a0f86de1f3c959ada96dee5cf10bd52df5
Nishhh <nishthamaheshwari123@gmail.com>
2021-07-23 18:40:00 +0530

    firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyDcwoYL58qYr_Qgs0JF92aH7yX1GroFR28",
+    authDomain: "chat-web-app-581c0.firebaseapp.com",
+    databaseURL: "https://chat-web-app-581c0-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-581c0",
+    storageBucket: "chat-web-app-581c0.appspot.com",
+    messagingSenderId: "998274567484",
+    appId: "1:998274567484:web:99262f5a46ab3b6cf5e239"
+};
+
+const app = firebase.initializeApp(config);
